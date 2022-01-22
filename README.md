5f6d9bd8a5137ec86a006d4fc27372d67f5cfb39
WEBundleXYZ <stefg@webundlexyzsmbp.mynetworksettings.com>
2022-01-22 09:05:15 -0500

    Storing data in files

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 [1 - Express server and handling req and res ](../../tree/3059049edf9579ca3f4c78a6845a4582892aee07/)
 
-[1 - Empty ](../../tree//)
+[1 - Parsing the user request ](../../tree/62681ee02c6bdb8e31daed01e7384db4503a1f09/)
 
 [1 - Empty ](../../tree//)
 
--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 
 const app = express();
@@ -15,8 +17,30 @@ app.get('/', function (req, res) {
 });
 
 app.post('/store-user', function (req, res) {
+
+	// The user input entered by the user, already translated (parsed) in JS code with middleware 'app.use' 
 	const userName = req.body.username;
-	console.log(userName);
+
+	// Defines the path: the overall path --> absolute path - tells where the file is
+	// dirname/data/users.json --> cross platforms on any operating system
+	const filePath = path.join(__dirname, 'data', 'users.json'); 
+
+	// The file data interpreted as text (not JSON or JS)
+	// this data must be transformed to work with it
+	const fileData = fs.readFileSync(filePath);
+
+  //The text data, that actually follows the JSON format, must be translated in a JS object or array
+	// 'parse' - transforms the raw data (the text) in JS object or array
+	const existingUsers = JSON.parse(fileData);
+
+	//pushing the 'userName' into the 'existingUsers'
+	existingUsers.push(userName);
+
+	// Sync writes the file instantly 
+	// 'writeFileSync' = path + userName (but username is raw data that must be transformed to be able to work with it)
+	// 'writeFileSync' needs raw text, therefore, the 'existingUsers' must be transformed back into raw text
+	fs.writeFileSync(filePath, JSON.stringify(existingUsers)); 
+
 	res.send('<h1>Username stored!</h1>');
 });
 
